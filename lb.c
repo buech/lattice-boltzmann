1b74588211457cefbd486441fee3f14c2c83075f
Adam Buechner <buechner.adam@gmx.de>
2018-11-10 17:25:05 +0100

    Refactor C code

--- lb.c
+++ lb.c
@@ -46,10 +46,6 @@ static const int c[9][2] = {
 
 static const int noslip[9] = {0, 2, 1, 6, 8, 7, 3, 5, 4};
 
-struct vec {
-   double x, y;
-};
-
 static inline int idx(int i, int j) {
    int id = M * i + j;
    assert(id >= 0);
@@ -57,69 +53,8 @@ static inline int idx(int i, int j) {
    return id;
 }
 
-void update_rho(double* restrict rho, double* restrict f) {
-#pragma omp parallel for
-   for (int i = 0; i < N*M; i++) {
-      double sum = 0;
-      for (int q = 0; q < 9; q++)
-         sum += f[9*i + q];
-
-      rho[i] = sum;
-   }
-}
-
-void update_u(struct vec * restrict u, double* restrict rho, double* restrict f) {
-#pragma omp parallel for
-   for (int i = 0; i < N*M; i++) {
-      double sum1 = 0, sum2 = 0;
-      for (int q = 0; q < 9; q++) {
-         sum1 += f[9*i + q] * c[q][0];
-         sum2 += f[9*i + q] * c[q][1];
-      }
-
-      u[i].x = sum1 / rho[i];
-      u[i].y = sum2 / rho[i];
-   }
-}
-
-void update_feq(double* restrict feq, double* restrict rho, struct vec * restrict u, double cs) {
-   double oocs2 = 1. / (cs*cs);
-   double oocs4 = oocs2 * oocs2;
-#pragma omp parallel for collapse(2)
-   for (int i = 0; i < N*M; i++) {
-      for (int q = 0; q < 9; q++) {
-         double cu = c[q][0] * u[i].x + c[q][1] * u[i].y;
-         double cu2 = cu * cu;
-         double u2 = u[i].x * u[i].x + u[i].y * u[i].y;
-
-         feq[9*i + q] = w[q] * rho[i]
-            * (1 + oocs2 * cu + 0.5 * oocs4 * cu2 - 0.5 * oocs2 * u2);
-      }
-   }
-}
-
-void collide(double* restrict ft, double* restrict f, double* restrict feq, int obstacle[], double omega) {
-#pragma omp parallel for collapse(2)
-   for (int i = 0; i < N*M; i++) {
-      for (int q = 0; q < 9; q++) {
-         int id = 9*i + q;
-         if (!obstacle[i])
-            ft[id] = f[id] - omega * (f[id] - feq[id]);
-         else
-            ft[id] = f[9*i + noslip[q]];
-      }
-   }
-}
-
-void stream(double* restrict f, double* restrict ft) {
-#pragma omp parallel for collapse(3)
-   for (int i = 0; i < N; i++) {
-      for (int j = 0; j < M; j++) {
-         for (int q = 0; q < 9; q++) {
-            f[9*idx((N + (i + c[q][0]) % N) % N, (M + (j + c[q][1]) % M) % M) + q] = ft[9*idx(i, j) + q];
-         }
-      }
-   }
+static inline int mod(int a, int b) {
+   return (b + a%b) % b;
 }
 
 static inline double inlet_vel(int k, int i, double ulb) {
@@ -127,19 +62,43 @@ static inline double inlet_vel(int k, int i, double ulb) {
    return (1. - k) * ulb * (1. + 1.e-4 * sin(2.*M_PI * x));
 }
 
-void update(double* restrict f, double* restrict ft, double* restrict feq, double* restrict rho, struct vec * restrict u, int obstacle[], double cs, double omega, double ulb) {
+double rho(int i, int j, double* restrict f) {
+   double sum = 0;
+   for (int q = 0; q < 9; q++)
+      sum += f[9*idx(i,j) + q];
+
+   return sum;
+}
+
+double u(int k, int i, int j, double* restrict f) {
+   double sum = 0;
+   for (int q = 0; q < 9; q++)
+      sum += f[9*idx(i,j) + q] * c[q][k];
+
+   return sum / rho(i,j, f);
+}
+
+double feq(int i, int j, int q, double* restrict f) {
+   double u_x = u(0,i,j, f);
+   double u_y = u(1,i,j, f);
+
+   double cu = c[q][0] * u_x + c[q][1] * u_y;
+   double u2 = u_x * u_x + u_y * u_y;
+
+   return w[q] * rho(i,j, f) * (1 + 3 * cu + 0.5 * 9 * cu*cu - 0.5 * 3 * u2);
+}
+
+void boundary(double* restrict f, double ulb) {
    // outflow
-#pragma omp parallel for collapse(2)
    for (int j = 0; j < M; j++)
       for (int q = 3; q < 6; q++)
          f[9*idx(N-1, j) + q] = f[9*idx(N-2, j) + q];
 
-   update_rho(rho, f);
-   update_u(u, rho, f);
-
    // inflow
-#pragma omp parallel for
    for (int j = 0; j < M; j++) {
+      double u_x = inlet_vel(0, j, ulb);
+      double u_y = inlet_vel(1, j, ulb);
+      double u2 = u_x*u_x + u_y*u_y;
       double sum1 = 0, sum2 = 0;
       for (int q = 0; q < 6; q++) {
          if (q < 3)
@@ -147,54 +106,64 @@ void update(double* restrict f, double* restrict ft, double* restrict feq, doubl
          else
             sum1 += f[9*j + q];
       }
-      u[j].x = inlet_vel(0, j, ulb);
-      u[j].y = inlet_vel(1, j, ulb);
 
-      rho[j] = 1. / (1. - u[j].x) * (sum2 + 2. * sum1);
+      double rho_j = 1. / (1 - u_x) * (sum2 + 2*sum1);
+
+      for (int q = 6; q < 9; q++) {
+         double cu = c[q][0] * u_x + c[q][1] * u_y;
+         f[9*j + q] = w[q] * rho_j * (1 + 3 * cu + 0.5 * 9 * cu*cu - 0.5 * 3 * u2);
+      }
    }
-   update_feq(feq, rho, u, cs);
-#pragma omp parallel for collapse(2)
-   for (int j = 0; j < M; j++)
-      for (int q = 6; q < 9; q++)
-         f[9*j + q] = feq[9*j + q];
-
-   collide(ft, f, feq, obstacle, omega);
-   stream(f, ft);
 }
 
-void print_u(struct vec * restrict u, int t) {
-   printf("%d", t);
-   for (int i = 0; i < N*M; i++) {
-      printf(" %E", sqrt(u[i].x * u[i].x + u[i].y * u[i].y));
+void collstream(double* restrict fnew, double* restrict fold, int* restrict obstacle, double omega) {
+#pragma omp parallel for collapse(3)
+   for (int i = 0; i < N; i++) {
+      for (int j = 0; j < M; j++) {
+         for (int q = 0; q < 9; q++) {
+            int id = 9*idx(i,j) + q;
+            int idnew = 9*idx(mod(i + c[q][0], N), mod(j + c[q][1], M)) + q;
+            if (!obstacle[idx(i,j)])
+               fnew[idnew] = (1 - omega) * fold[id] + omega*feq(i,j,q, fold);
+            else
+               fnew[idnew] = fold[9*idx(i,j) + noslip[q]];
+         }
+      }
    }
-   putchar('\n');
 }
 
-void print_rho(double* restrict rho, int t) {
+void update(double* restrict fnew, double* restrict fold, int* restrict obstacle, double omega, double ulb) {
+   collstream(fnew, fold, obstacle, omega);
+   boundary(fnew, ulb);
+}
+
+static inline void print_u(double* restrict f, int t) {
+   if (t % 100)
+      return;
+
    printf("%d", t);
-   for (int i = 0; i < N*M; i++) {
-      printf(" %E", rho[i]);
+   for (int i = 0; i < N; i++) {
+      for (int j = 0; j < M; j++) {
+         double ux = u(0, i, j, f);
+         double uy = u(1, i, j, f);
+         printf(" %E", sqrt(ux*ux + uy*uy));
+      }
    }
    putchar('\n');
 }
 
 int main() {
-   double cs = 1./sqrt(3.);
    double Re = 160.;
    double ulb = 0.04;
    double nu = ulb * r / Re;
    double omega = 1. / (3. * nu + 0.5);
 
-   double* restrict f = malloc(9 * N * M * sizeof(double));
-   double* restrict ft = malloc(9 * N * M * sizeof(double));
-   double* restrict feq = malloc(9 * N * M * sizeof(double));
-   double* restrict rho = malloc(N * M * sizeof(double));
-   struct vec* restrict u = malloc(N * M * sizeof(struct vec));
+   double* restrict fnew = malloc(9 * N * M * sizeof(double));
+   double* restrict fold = malloc(9 * N * M * sizeof(double));
 
    int* restrict obstacle = malloc(N*M * sizeof(int));
    for (int i = 0; i < N; i++) {
       for (int j = 0; j < M; j++) {
-         rho[M*i + j] = 1.;
          if ((i-cx)*(i-cx) + (j-cy)*(j-cy) < r*r)
             obstacle[idx(i, j)] = 1;
          else
@@ -204,15 +173,15 @@ int main() {
 
    for (int i = 0; i < N; i++) {
       for (int j = 0; j < M; j++) {
-         u[idx(i, j)].x = inlet_vel(0, j, ulb);
-         u[idx(i, j)].y = inlet_vel(1, j, ulb);
+         double ux = inlet_vel(0, j, ulb);
+         double uy = inlet_vel(1, j, ulb);
+         double u2 = ux*ux + uy*uy;
+         for (int q = 0; q < 9; q++) {
+            double cu = c[q][0] * ux + c[q][1] * uy;
+            fold[9*idx(i,j) + q] = w[q] * 1.0 * (1 + 3 * cu + 0.5 * 9 * cu*cu - 0.5 * 3 * u2);
+         }
       }
    }
-
-   update_feq(feq, rho, u, cs);
-   for (int i = 0; i < N*M; i++)
-      for (int q = 0; q < 9; q++)
-         f[9*i + q] = feq[9*i + q];
 
    int T = 20000;
    //int barsteps = (int)ceil(T / 100.);
@@ -236,18 +205,17 @@ int main() {
          fflush(stdout);
       }
       */
-      if (!(t % 100)) {
-         print_u(u, t);
-         //print_rho(rho, t);
+      if (t % 2) {
+         print_u(fnew, t);
+         update(fold, fnew, obstacle, omega, ulb);
+      } else {
+         print_u(fold, t);
+         update(fnew, fold, obstacle, omega, ulb);
       }
-      update(f, ft, feq, rho, u, obstacle, cs, omega, ulb);
    }
 
-   free(f);
-   free(ft);
-   free(feq);
-   free(rho);
-   free(u);
+   free(fnew);
+   free(fold);
    free(obstacle);
 
    return 0;
